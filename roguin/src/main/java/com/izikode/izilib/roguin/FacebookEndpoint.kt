6d27f483aac6c3157ad8c39911dff9fad443a022
iFanie <fanieveizis@gmail.com>
2018-11-25 22:49:34 +0200

    Implement login & logout for facebook endpoint

--- roguin/src/main/java/com/izikode/izilib/roguin/FacebookEndpoint.kt
+++ roguin/src/main/java/com/izikode/izilib/roguin/FacebookEndpoint.kt
@@ -1,13 +1,65 @@
 package com.izikode.izilib.roguin
 
-import com.facebook.AccessToken
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
 
-class FacebookEndpoint : RoguinEndpoint {
+class FacebookEndpoint(
+
+        private val facebookLoginButton: LoginButton
+
+) : RoguinEndpoint {
 
     override val isSignedIn: Boolean
         get() {
             val accessToken = AccessToken.getCurrentAccessToken()
             return accessToken != null && !accessToken.isExpired
         }
+
+    override fun requestSignIn(response: (success: Boolean, result: RoguinProfile?, error: RoguinException?) -> Unit) {
+        facebookLoginButton.registerCallback(CallbackManager.Factory.create(), object : FacebookCallback<LoginResult> {
+
+            override fun onSuccess(result: LoginResult?) {
+                facebookLoginButton.removeCallbacks {}
+
+                if (result != null) {
+                    response.invoke(true, parseToProfile(result), null)
+                } else {
+                    response.invoke(false, null, null)
+                }
+            }
+
+            override fun onCancel() {
+                facebookLoginButton.removeCallbacks {}
+                response.invoke(false, null, null)
+            }
+
+            override fun onError(error: FacebookException?) {
+                facebookLoginButton.removeCallbacks {}
+                response.invoke(false, null, RoguinException(error))
+            }
+
+        })
+
+        facebookLoginButton.performClick()
+    }
+
+    private fun parseToProfile(facebookLoginResult: LoginResult) = RoguinProfile().apply {
+        /* TODO actually parse */
+    }
+
+    override fun requestSignOut(response: (success: Boolean) -> Unit) {
+        GraphRequest(AccessToken.getCurrentAccessToken(), "/me/permissions/", null, HttpMethod.DELETE, GraphRequest.Callback {
+
+            if (it.error == null) {
+                LoginManager.getInstance().logOut()
+                response.invoke(true)
+            } else {
+                response.invoke(false)
+            }
+
+        }).executeAsync()
+    }
 
 }
