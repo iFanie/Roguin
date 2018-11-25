121a624f545e07c01f2eb7f81b0a45a7c064df52
iFanie <fanieveizis@gmail.com>
2018-11-25 22:46:34 +0200

    Create generic profile and exception classes
    - RoguinProfile
    A class that will unify the login responses of the endpoints
    
    - RoguinException
    A class that will represent errors in the login-logout processes

---
+++ roguin/src/main/java/com/izikode/izilib/roguin/RoguinException.kt
@@ -0,0 +1,18 @@
+package com.izikode.izilib.roguin
+
+import android.content.Intent
+import java.lang.Exception
+
+data class RoguinException(
+
+        /**
+         * The error that produced the Exception.
+         */
+        val internalException: Exception? = null,
+
+        /**
+         * The unsuccessful data that produced the Exception.
+         */
+        val internalIntent: Intent? = null
+
+) : Exception(internalException)
---
+++ roguin/src/main/java/com/izikode/izilib/roguin/RoguinProfile.kt
@@ -0,0 +1,4 @@
+package com.izikode.izilib.roguin
+
+class RoguinProfile {
+}
